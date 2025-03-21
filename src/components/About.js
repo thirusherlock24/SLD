5a72e2a0bcd758fd1529c2c9b41b779e5609c2dd
Thirunavukarasu L <thirusherlock@gmail.com>
2025-03-21 20:45:03 +0530

    Done changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,38 @@
+import img from "../Images/BPCL.jpeg";
+import "./About.css";
+
 function About() {
-    return (
-      <section style={{ background: "#fff", boxShadow: "0 0 10px rgba(0,0,0,0.05)" }}>
+  return (
+    <section className="about-section">
+      <div className="about-content">
         <h2>About Us</h2>
-        <p><strong>SLD PETROLEUM FUEL ENTERPRISES</strong> is the fuel delivery tanker truck called as the <strong>(BROWSER)</strong>. It is a specialized tanker capacity of 6KL designed for transportation and supply of fuel (HSD). Our aim is to deliver fuel to the internal combustion engines to various Industries, Fishing boats, construction sites, agriculture, harbors etc., and also to schools, colleges, and hospitals for use in diesel generators.</p>
-        <br />
-        <p>SLD PETROLEUM FUEL ENTERPRISES is a partnership company owned by <strong>M. SATHIAVELU</strong> and <strong>S. DHEEBAN</strong>. Our main fuel partner is <strong>BPCL</strong>. The bowser truck is equipped with advanced safety features including a spill containment system, fire suppression equipment, PV valve, emergency valve, venting system, fusible link and fully complies with <strong>PESO</strong> regulations.</p>
-      </section>
-    );
-  }
-  export default About;
+        <p>
+          <strong>SLD PETROLEUM FUEL ENTERPRISES</strong> is the fuel delivery
+          tanker truck called the <strong>(BROWSER)</strong>. It is a
+          specialized tanker with a capacity of 6KL designed for transportation
+          and supply of fuel (HSD). Our aim is to deliver fuel to the internal
+          combustion engines of various industries, fishing boats, construction
+          sites, agriculture, harbors, etc., and also to schools, colleges, and
+          hospitals for use in diesel generators.
+        </p>
+
+        <p>
+          SLD PETROLEUM FUEL ENTERPRISES is a partnership company owned by{" "}
+          <strong>M. SATHIAVELU</strong> and <strong>S. DHEEBAN</strong>. Our
+          main fuel partner is <strong>BPCL</strong>. The bowser truck is
+          equipped with advanced safety features including a spill containment
+          system, fire suppression equipment, PV valve, emergency valve,
+          venting system, fusible link and fully complies with{" "}
+          <strong>PESO</strong> regulations.
+        </p>
+
+        <h2 className="partner-heading">Our beloved partner: BPCL</h2>
+        <div className="logo-container">
+          <img src={img} alt="BPCL Logo" className="partner-logo" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
